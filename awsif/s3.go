7ca7f37745ff488c25a33bd1acdf5930e4d541eb
Florian Wehling <florian.wehling@mansio-logistics.com>
2023-10-09 13:58:24 +0200

    Add GetObjectAttributes to S3-awsif

--- awsif/s3.go
+++ awsif/s3.go
@@ -9,4 +9,5 @@ import (
 type S3Client interface {
 	GetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
 	PutObject(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error)
+	GetObjectAttributes(ctx context.Context, params *s3.GetObjectAttributesInput, optFns ...func(*s3.Options)) (*s3.GetObjectAttributesOutput, error)
 }
