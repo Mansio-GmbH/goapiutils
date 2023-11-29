b556227e9f77e6222dc3914968296f2dffce0c0c
Florian Wehling <florian.wehling@mansio-logistics.com>
2023-11-29 20:50:16 +0100

    Add CoordinatesWithTimestamp

--- ct/coordinates.go
+++ ct/coordinates.go
@@ -1,6 +1,13 @@
 package ct
 
+import "github.com/mansio-gmbh/goapiutils/chrono"
+
 type Coordinates struct {
 	Latitude  float64 `json:"latitude" dynamodbav:"latitude" validate:"gte=-90,lte=90"`
 	Longitude float64 `json:"longitude" dynamodbav:"longitude" validate:"gte=-180,lte=180"`
+}
+
+type CoordinatesWithTimestamp struct {
+	Coordinates
+	Timestamp *chrono.Time `json:"timestamp" dynamodbav:"timestamp"`
 }
