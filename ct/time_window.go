bba0ea844e109a1c2253ebba2f8a375e4608720d
Florian Wehling <florian.wehling@mansio-logistics.com>
2023-12-15 22:51:53 +0100

    Add time window

---
+++ ct/time_window.go
@@ -0,0 +1,8 @@
+package ct
+
+import "github.com/mansio-gmbh/goapiutils/chrono"
+
+type TimeWindow struct {
+	StartsAt chrono.Time `json:"startsAt" dynamodbav:"startsAt" validate:"required"`
+	EndsAt   chrono.Time `json:"endsAt" dynamodbav:"endsAt" validate:"required,gtcsfield=StartsAt"`
+}
