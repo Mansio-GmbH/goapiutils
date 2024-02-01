99a1a73da8ffc213705bbfcb78aa40d02f2a1bb1
Florian Wehling <florian.wehling@mansio-logistics.com>
2024-02-01 16:05:17 +0100

    Add ToQueryInput to QueryConfig

--- queryconfigv2/queryconfigv2.go
+++ queryconfigv2/queryconfigv2.go
@@ -1,6 +1,10 @@
 package queryconfigv2
 
-import "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
 
 type QueryConfig struct {
 	TableName                 *string
@@ -11,4 +15,17 @@ type QueryConfig struct {
 	ExpressionAttributeValues map[string]types.AttributeValue
 	ExclusiveStartKey         map[string]types.AttributeValue
 	ReverseOrder              bool
+}
+
+func (qc QueryConfig) ToQueryInput() *dynamodb.QueryInput {
+	return &dynamodb.QueryInput{
+		TableName:                 qc.TableName,
+		IndexName:                 qc.IndexName,
+		KeyConditionExpression:    qc.KeyConditionExpression,
+		ExpressionAttributeValues: qc.ExpressionAttributeValues,
+		ExclusiveStartKey:         qc.ExclusiveStartKey,
+		FilterExpression:          qc.FilterExpression,
+		Limit:                     qc.Limit,
+		ScanIndexForward:          aws.Bool(!qc.ReverseOrder),
+	}
 }
