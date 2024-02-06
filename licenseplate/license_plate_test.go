963a84fd9b11eb0ab255c19e49c9fb4ba1e0386f
Parth Hareshbhai Saliya <parth.saliya@mansio.org>
2024-02-06 14:41:17 +0100

    DyanmoDB test

--- licenseplate/license_plate_test.go
+++ licenseplate/license_plate_test.go
@@ -23,3 +23,21 @@ func TestLicensePlate_UnmarshalJSON(t *testing.T) {
 		t.Errorf("Failed to unmarshal LicensePlate: %v", err)
 	}
 }
+
+func TestLicensePlate_MarshalDynamoDBAttributeValue(t *testing.T) {
+	lp := licenseplate.LicensePlate{
+		LicensePlate: []string{"ABC", "123"},
+	}
+	_, err := lp.MarshalDynamoDBAttributeValue()
+	if err != nil {
+		t.Errorf("Failed to marshal LicensePlate: %v", err)
+	}
+}
+
+func TestLicensePlate_UnmarshalDynamoDBAttributeValue(t *testing.T) {
+	lp := &licenseplate.LicensePlate{}
+	err := lp.UnmarshalDynamoDBAttributeValue(nil)
+	if err != nil {
+		t.Errorf("Failed to unmarshal LicensePlate: %v", err)
+	}
+}
