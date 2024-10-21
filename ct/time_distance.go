973a1482f7ce06a0041bee84f8d3afe58abb7886
Morten Urban <morten.urban@mansio-logistics.com>
2024-10-21 12:27:01 +0200

    added time_distance json marshal functions

--- ct/time_distance.go
+++ ct/time_distance.go
@@ -1,16 +1,18 @@
 package ct
 
 import (
+	"encoding/json"
 	"errors"
 	"math"
+	"strconv"
 )
 
 var ErrInvalidTimeDistance = errors.New("invalid time distance")
 
 type TimeDistance struct {
-	DurationS int      `json:"duration_s"`
-	DistanceM Distance `json:"distance_m"`
-	Problem   error    `json:"problem,omitempty"`
+	DurationS int
+	DistanceM Distance
+	Problem   error
 }
 
 // Invalidate sets the TimeDistance to invalid and sets the duration and distance to the maximum possible value.
@@ -19,4 +21,36 @@ func (t *TimeDistance) Invalidate() {
 	t.Problem = ErrInvalidTimeDistance
 	t.DurationS = math.MaxInt
 	t.DistanceM = Distance(math.MaxInt64)
+}
+
+func (t *TimeDistance) MarshalJSON() ([]byte, error) {
+	start := `{"duration_s":` + strconv.Itoa(t.DurationS) + `,"distance_m":` + strconv.FormatInt(int64(t.DistanceM), 10)
+	if t.Problem != nil {
+		return []byte(start + `",problem":"` + t.Problem.Error() + `"}`), nil
+	}
+	return []byte(start + `}`), nil
+}
+
+func (t *TimeDistance) UnmarshalJSON(b []byte) error {
+	tmp := struct {
+		DurationS int      `json:"duration_s"`
+		DistanceM Distance `json:"distance_m"`
+		Problem   string   `json:"problem,omitempty"`
+	}{}
+
+	err := json.Unmarshal(b, &tmp)
+	if err != nil {
+		return errors.New("unable to unmarshal time distance")
+	}
+
+	t.DurationS = tmp.DurationS
+	t.DistanceM = tmp.DistanceM
+	if tmp.Problem != "" {
+		if tmp.Problem == ErrInvalidTimeDistance.Error() {
+			t.Problem = ErrInvalidTimeDistance
+		} else {
+			t.Problem = errors.New(tmp.Problem)
+		}
+	}
+	return nil
 }
