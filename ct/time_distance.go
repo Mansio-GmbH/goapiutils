5a75dc35aba4727f6a9e28dc19b36be58973b326
Morten Urban <158455145+MortenUrban@users.noreply.github.com>
2024-10-21 13:49:18 +0200

    added time_distance json marshal functions (#10)
    * added time_distance json marshal functions
    
    * added time_distance changed json marshal
    
    * added time_distance changed snake to camel case

--- ct/time_distance.go
+++ ct/time_distance.go
@@ -1,6 +1,7 @@
 package ct
 
 import (
+	"encoding/json"
 	"errors"
 	"math"
 )
@@ -8,9 +9,15 @@ import (
 var ErrInvalidTimeDistance = errors.New("invalid time distance")
 
 type TimeDistance struct {
-	DurationS int      `json:"duration_s"`
-	DistanceM Distance `json:"distance_m"`
-	Problem   error    `json:"problem,omitempty"`
+	DurationS int
+	DistanceM Distance
+	Problem   error
+}
+
+type timeDistanceJson struct {
+	DurationS int      `json:"durationS"`
+	DistanceM Distance `json:"distanceM"`
+	Problem   string   `json:"problem,omitempty"`
 }
 
 // Invalidate sets the TimeDistance to invalid and sets the duration and distance to the maximum possible value.
@@ -19,4 +26,35 @@ func (t *TimeDistance) Invalidate() {
 	t.Problem = ErrInvalidTimeDistance
 	t.DurationS = math.MaxInt
 	t.DistanceM = Distance(math.MaxInt64)
+}
+
+func (t *TimeDistance) MarshalJSON() ([]byte, error) {
+	tmp := timeDistanceJson{
+		DurationS: t.DurationS,
+		DistanceM: t.DistanceM,
+	}
+	if t.Problem != nil {
+		tmp.Problem = t.Problem.Error()
+	}
+
+	return json.Marshal(tmp)
+}
+
+func (t *TimeDistance) UnmarshalJSON(b []byte) error {
+	var tmp timeDistanceJson
+	err := json.Unmarshal(b, &tmp)
+	if err != nil {
+		return errors.New("unable to unmarshal time distance")
+	}
+
+	t.DurationS = tmp.DurationS
+	t.DistanceM = tmp.DistanceM
+	if tmp.Problem != "" {
+		if tmp.Problem == ErrInvalidTimeDistance.Error() {
+			t.Problem = ErrInvalidTimeDistance
+		} else {
+			t.Problem = errors.New(tmp.Problem)
+		}
+	}
+	return nil
 }
