4de60f0fa9fc0ae27777985ff82b69885f75f9bb
Florian Wehling <florian.wehling@mansio-logistics.com>
2025-02-14 13:27:02 +0100

    Update tourqry TourFilter

--- datalake/toursqry/filter.go
+++ datalake/toursqry/filter.go
@@ -1,17 +1,19 @@
 package toursqry
 
+import "github.com/mansio-gmbh/goapiutils/chrono"
+
 type FromUntil struct {
-	From  *string `json:"from"`
-	Until *string `json:"until"`
+	From  chrono.Date `json:"from"`
+	Until chrono.Date `json:"until"`
 }
 
 type TourFilter struct {
-	TenantID                 *string    `json:"tenantID"`
-	TourDate                 *FromUntil `json:"tourDate"`
-	PickupCountryCode        *string    `json:"pickupCountryCode"`
-	DeliveryCountryCode      *string    `json:"deliveryCountryCode"`
-	PickupPostalCodePrefix   *string    `json:"pickupPostalCodePrefix"`
-	DeliveryPostalCodePrefix *string    `json:"deliveryPostalCodePrefix"`
+	TenantID                 string    `json:"tenantID"`
+	TourDate                 FromUntil `json:"tourDate"`
+	PickupCountryCode        string    `json:"pickupCountryCode"`
+	DeliveryCountryCode      string    `json:"deliveryCountryCode"`
+	PickupPostalCodePrefix   string    `json:"pickupPostalCodePrefix"`
+	DeliveryPostalCodePrefix string    `json:"deliveryPostalCodePrefix"`
 }
 
 type Filters []TourFilter
