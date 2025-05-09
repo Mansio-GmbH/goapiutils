af8b3597b2a2de8be52a3afe4aa9f6cf81ce5a6d
Florian Wehling <florian.wehling@mansio-logistics.com>
2025-05-09 10:36:41 +0200

    Datalake-Options: Add option to filter tours without in or out depot

--- datalake/toursqry/options.go
+++ datalake/toursqry/options.go
@@ -14,11 +14,12 @@ const (
 )
 
 type Options struct {
-	DistanceMode               string   `json:"distanceMode"`                       // "none", "haversine", "real"
-	DepotLoadingMode           string   `json:"depotLoadingMode"`                   // "none", "owner", "all", "byTenant"
-	HandoverStationLoadingMode string   `json:"handoverStationLoadingMode"`         // "none", "all", "byTenant"
-	DepotTenantIDs             []string `json:"depotTenantIDs,omitempty"`           // only used if DepotLoadingMode == "byTenant"
-	HandoverStationTenantIDs   []string `json:"handoverStationTenantIDs,omitempty"` // only used if HandoverStationLoadingMode == "byTenant"
-	Shuffle                    bool     `json:"shuffle"`
-	ShuffleSeed                int64    `json:"shuffleSeed"`
+	DistanceMode                   string   `json:"distanceMode"`                       // "none", "haversine", "real"
+	DepotLoadingMode               string   `json:"depotLoadingMode"`                   // "none", "owner", "all", "byTenant"
+	HandoverStationLoadingMode     string   `json:"handoverStationLoadingMode"`         // "none", "all", "byTenant"
+	DepotTenantIDs                 []string `json:"depotTenantIDs,omitempty"`           // only used if DepotLoadingMode == "byTenant"
+	HandoverStationTenantIDs       []string `json:"handoverStationTenantIDs,omitempty"` // only used if HandoverStationLoadingMode == "byTenant"
+	Shuffle                        bool     `json:"shuffle"`
+	ShuffleSeed                    int64    `json:"shuffleSeed"`
+	FilterToursWithoutInOrOutDepot bool     `json:"filterToursWithoutInOrOutDepot"` // if true, filter out tours without in or out depot
 }
