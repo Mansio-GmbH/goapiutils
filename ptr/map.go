b499d631e850e583fed834c38b6faa1e765bfc8b
Florian Wehling <florian.wehling@mansio-logistics.com>
2025-12-10 14:57:04 +0100

    Add NotNil and IsNil to ptr package

--- ptr/map.go
+++ ptr/map.go
@@ -6,3 +6,11 @@ func MapNilIfEmpty[K comparable, T any](m map[K]T) map[K]T {
 	}
 	return m
 }
+
+func NotNil[T any](v *T) bool {
+	return v != nil
+}
+
+func IsNil[T any](v *T) bool {
+	return v == nil
+}
