14940a8827cbb2fcc1c645a1daf22be505a676a4
Morten Urban <158455145+MortenUrban@users.noreply.github.com>
2026-02-06 16:25:21 +0100

    added performant outsideBoundingBox func to coordinate (#15)

--- ct/coordinates.go
+++ ct/coordinates.go
@@ -7,6 +7,8 @@ import (
 	"github.com/mansio-gmbh/goapiutils/hash"
 )
 
+const earthRadius = 6371.0
+
 type Coordinates struct {
 	Latitude  float64 `json:"latitude" dynamodbav:"latitude" validate:"gte=-90,lte=90"`
 	Longitude float64 `json:"longitude" dynamodbav:"longitude" validate:"gte=-180,lte=180"`
@@ -22,7 +24,6 @@ func (c Coordinates) IsZero() bool {
 }
 
 func (c Coordinates) HaversineDistance(other Coordinates) float64 {
-	const earthRadius = 6371.0
 	lat1 := c.Latitude
 	lon1 := c.Longitude
 	lat2 := other.Latitude
@@ -36,13 +37,30 @@ func (c Coordinates) HaversineDistance(other Coordinates) float64 {
 	deltaLat := lat2Rad - lat1Rad
 	deltaLon := lon2Rad - lon1Rad
 
-	a := (math.Sin(deltaLat/2)*math.Sin(deltaLat/2) +
+	a := math.Sin(deltaLat/2)*math.Sin(deltaLat/2) +
 		math.Cos(lat1Rad)*math.Cos(lat2Rad)*
-			math.Sin(deltaLon/2)*math.Sin(deltaLon/2))
+			math.Sin(deltaLon/2)*math.Sin(deltaLon/2)
 
 	dc := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
 
 	return earthRadius * dc
+}
+
+// OutsideBoundingBox checks if the coordinates are outside a bounding box defined by a center and a radius in km.
+// It calculates the latitude and longitude differences based on the radius and checks if the coordinates fall outside the bounding box.
+// It is only safe that the coordinates are outside the bounding box
+// but its is not safe the coordinates are inside the bounding box, because the bounding box is a square and the area
+// of interest is a circle.
+// This method is useful for quickly filtering out coordinates that are definitely outside a certain radius
+// without having to calculate the exact distance.
+func (c Coordinates) OutsideBoundingBox(center Coordinates, radiusKm float64) bool {
+	latDiff := radiusKm / earthRadius * (180 / math.Pi)
+	lonDiff := latDiff / math.Cos(center.Latitude*math.Pi/180)
+
+	return !(c.Latitude >= center.Latitude-latDiff &&
+		c.Latitude <= center.Latitude+latDiff &&
+		c.Longitude >= center.Longitude-lonDiff &&
+		c.Longitude <= center.Longitude+lonDiff)
 }
 
 func (c Coordinates) HaversineDistanceAsKm(other Coordinates) *UnitValue {
